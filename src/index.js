25e0a41cb254b4161f8bb33de25c90c083b4c1e1
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-09-13 23:16:56 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskList = document.querySelector("#tasks");
+    const newTask = document.createElement("li");
+    const text = event.target['new-task-description'].value;
+    newTask.textContent = text;
+
+    const priority = event.target['new-task-priority'].value;
+    if (priority === "high") newTask.style.backgroundColor = "red";
+    if (priority === "medium") newTask.style.backgroundColor = "yellow";
+    if (priority === "low") newTask.style.backgroundColor = "green";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    newTask.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", () => {
+      newTask.remove();
+    });
+
+    taskList.appendChild(newTask);
+
+    event.target.reset();
+  });
 });
